85a7bb8e968b59a2f3714b0a25f0955f1868d9f0
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-04-03 16:23:21 +0200

    feat: Added base folders

---
+++ mobile/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Activities.js</Text>
+      <Button onPress={() => alert('Login')} title="Login" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
