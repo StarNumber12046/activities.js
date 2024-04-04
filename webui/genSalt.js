69b3cf021b20e9a594e70b2b6b9cae37b5950a0f
Restyled.io <commits@restyled.io>
2024-04-04 16:12:26 +0000

    Restyled by prettier

--- webui/genSalt.js
+++ webui/genSalt.js
@@ -1,5 +1,10 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-bcrypt.genSalt(10)
-    .then((salt) => { console.log(Buffer.from(salt).toString('base64')); })
-    .catch((err) => { console.error(err); });
+bcrypt
+  .genSalt(10)
+  .then((salt) => {
+    console.log(Buffer.from(salt).toString("base64"));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
