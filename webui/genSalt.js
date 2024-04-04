9c478da306197973b6383a1ae72f8603ab3e38a5
Restyled.io <commits@restyled.io>
2024-04-04 16:12:20 +0000

    Restyled by clang-format

--- webui/genSalt.js
+++ webui/genSalt.js
@@ -1,10 +1,5 @@
 const bcrypt = require('bcrypt');
 
 bcrypt.genSalt(10)
-  .then((salt) => {
-    console.log(Buffer.from(salt).toString('base64'));
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
+    .then((salt) => { console.log(Buffer.from(salt).toString('base64')); })
+    .catch((err) => { console.error(err); });
