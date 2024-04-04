19221e3ea5e36ff81e82689145d32b6b61449c74
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-04-04 17:52:25 +0200

    feat(api): added API auth routes

---
+++ webui/genSalt.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+bcrypt.genSalt(10)
+  .then((salt) => {
+    console.log(Buffer.from(salt).toString('base64'));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
---
+++ webui/src/app/api/account/route.ts
@@ -0,0 +1,56 @@
+import "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const prisma = new PrismaClient({});
+
+interface Environment {
+  SALT: string;
+  JWT_SECRET: string;
+}
+
+declare let process: {
+  env: Environment;
+};
+
+const SALT = Buffer.from(process.env.SALT, 'base64').toString()
+
+export async function PUT(request: Request) {
+  if (!request.headers.get("email") || !request.headers.get("password")) {
+    return new Response("Missing email or password", { status: 400 });
+  }
+  const hashedPassword = bcrypt.hashSync(
+    request.headers.get("password") ?? "",
+    SALT
+  );
+  let user = await prisma.user.create({
+    data: {
+      email: request.headers.get("email")?.toString() ?? "",
+      password: hashedPassword,
+    },
+  });
+  if (user == null) {
+    return new Response("Invalid email or password", { status: 401 });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET || "",
+    { expiresIn: "1h" }
+  );
+
+  await prisma.authToken.create({
+    data: {
+      ownerId: user.id,
+      token: bcrypt.hashSync(token, SALT),
+    },
+  });
+  return new Response(JSON.stringify({ status: "success", email: user.email, token: token }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
