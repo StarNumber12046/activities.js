74c8eaafc65f90ed6970ec87d942b334e4ce0adb
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-04-05 16:00:02 +0200

    chore: added !

--- webui/src/app/api/login/route.ts
+++ webui/src/app/api/login/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: Request) {
     })
 
     if (user == null) {
-        return new Response('Invalid email or password', { status: 401 });
+        return new Response('Invalid email or password!', { status: 401 });
     }
 
     const token = jwt.sign({
